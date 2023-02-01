1715082530c5d686a2d3a96ea30c3db5d3788e81
Antonio <antonio.jdlsc@gmail.com>
2023-02-01 17:29:49 -0600

    José A. de los Santos. My thrid commit: Setting a slice for a better performance

--- api.js
+++ api.js
@@ -2,6 +2,11 @@
 // LLAMADA A LA API PARA DATOS DEL CANVA
 // chart solo puede crear un grafico por id sino da error por eso
 // tiene el id dinamico
+
+
+Chart.defaults.color = "black";
+
+
 const apiCanva = async (id) => {
     let response = await fetch(
       `https://api.coincap.io/v2/assets/${id}/history?interval=d1`
@@ -20,7 +25,6 @@ const apiCanva = async (id) => {
 
     try {
       if (data) {
-        Chart.defaults.color = "white";
         new Chart(document.getElementById(`${id}`), {
           type: "line",
           data: {
@@ -53,20 +57,20 @@ const apiCanva = async (id) => {
     try {
       if (data) {
       //  desestructuramos el objeto
-        data.data.forEach(
+        data.data.slice(0,40).forEach(
           ({ symbol, name, rank, priceUsd, supply, changePercent24Hr,id }) => {
             
             // creamos un div por cada elemento devuelto de la api añado el canvas
             const nuevoDiv = document.createElement('div')
             nuevoDiv.innerHTML = `
-            <div class='card'>
+            <div class='card__item'>
             <h6><span>Simbolo:</span> ${symbol}</h6>
             <h6><span>Nombre:</span> ${name}</h6>
             <h6><span>Rango:</span> ${rank}</h6>
             <h6><span>Precio:</span> ${priceUsd.slice(0,8)}</h6>
             <h6><span>Supply:</span> ${supply.slice(0,14)}</h6>
             <h6><span>Cambio 24h:</span> ${changePercent24Hr.slice(0,5)}</h6>
-            <canvas class="canvas" id=${id} width="200px" height="200px"></canvas>
+            <canvas class="canvas" id=${id}></canvas>
             </div>
             `;
             // siempre tenemos hay que decirle quien es el padre en 
